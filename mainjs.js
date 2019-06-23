42a677a10e961e5bc57a611ed4639796674a2650
Monika Nawrocka <monikanawrocka@onet.pl>
2019-06-24 01:40:56 +0200

    add some css

--- mainjs.js
+++ mainjs.js
@@ -1,3 +1,26 @@
+//four images abou weather
+$(document).ready(function() {
+    $('.background-phenomen-weather').mouseover(function() {
+      $(this).children('div').removeClass("hidden-not-found");
+    });
+
+    $('.background-phenomen-weather').mouseout(function() {
+      $('.phenomen').addClass('hidden-not-found');
+    });
+});
+
+$(document).ready(function() {
+    $('.background-phenomen-weather').mouseover(function() {
+      $(this).children('p').addClass("hidden-not-found");
+    });
+
+    $('.background-phenomen-weather').mouseout(function() {
+      $('p').removeClass("hidden-not-found");
+    });
+});
+
+
+
 //the weather
 
 function capitalizeFirstLetter(string) {
@@ -84,10 +107,43 @@ function getDateFromApi(data) {
        DateFromApi.push(data.list[i].dt_txt)
      };
       return DateFromApi;
-  };
+};
 
+function removingCharactersFromString(data) {
+  let reducedDate = []
+  getDateFromApi(data).forEach(function(element){
+    reducedDate.push(element.slice(5, -3));
+  })
+  return reducedDate;
+};
 
+function getMaxValue(data){
+   let maxTemp = Math.max.apply(null, temperaturesArrayinCelsiusRounded(data));
+    let roundedMaxTemp = Math.ceil(maxTemp / 10) * 10;
+     return roundedMaxTemp;
+};
 
+function getMinValue(data){
+   let minTemp = Math.min.apply(null, temperaturesArrayinCelsiusRounded(data));
+    let roundedMinTemp = Math.floor(minTemp / 10) * 10;
+    return roundedMinTemp;
+};
+
+function speedWindArray(data) {
+   let forecastSpeedWind = [];
+   for(let i = 0; i < data.list.length; i++){
+     forecastSpeedWind.push(data.list[i].wind.speed)
+   };
+    return forecastSpeedWind;
+};
+
+function pressureArray(data) {
+   let forecastPressure = [];
+   for(let i = 0; i < data.list.length; i++){
+     forecastPressure.push(data.list[i].main.pressure)
+   };
+    return forecastPressure;
+};
 
 function getWeatherCurrent(city) {
     let weatherAPI= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7ea00d73a83a2dc576cf68ab20a5f063"
@@ -106,7 +162,7 @@ function getWeatherCurrent(city) {
 
         success();
         foundCity();
-        $("#your-city").html("Today's weather in " + capitalizeFirstLetter(city));
+        $("#your-city").html("Current's weather in " + capitalizeFirstLetter(city));
         $("#cloud").html("cloudiness: " + data.weather[0].description);
         $("#temperature").html("temperature: " + roundNumber(calvinToCelsius(data.main.temp)) +'&#x2103;');
         $("#humidity").html("humidity: " + data.main.humidity + "%");
@@ -132,9 +188,13 @@ function getWeatherForecast(city) {
       success: function(data){
         success();
         foundCity();
-        console.log(temperaturesArrayinCelsiusRounded(data));
-        console.log(getDateFromApi(data));
+        getMaxValue(data)
+        console.log(pressureArray(data));
+        console.log(removingCharactersFromString(data));
+        console.log(speedWindArray(data));
+        getMinValue(data)
         chartForWeather(data);
+        chartForWeatherSecond(data);
       }
     });
     clearInput()
@@ -142,7 +202,6 @@ function getWeatherForecast(city) {
 
 
 $(document).ready(function() {
-  $('#inputCity').focus();
   $("#form-weather").submit(function(event){
     event.preventDefault();
     let city = $("#inputCity").val();
@@ -173,6 +232,8 @@ $(document).ready(function() {
   });
 });
 
+
+
 function chartForWeather(x){
     $(document).ready(function() {
       let myChart = document.getElementById('myChart').getContext('2d');
@@ -185,17 +246,33 @@ function chartForWeather(x){
       let weatherChart = new Chart (myChart, {
         type:'line',
         data: {
-          labels:['monika', 'wroclaw', 'warsaw', 'krakow', 'zosia', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+          labels:removingCharactersFromString(x),
           datasets:[{
             fill: false,
-            label:'population',
+            lineTension: 0.1,
+            borderColor: "rgb(165, 176, 52)",
+            pointBorderColor: "rgb(101, 106, 52)",
+            pointBackgroundColor: "rgba(101, 106, 52, 0.8)",
+            label:'temperature',
             data: temperaturesArrayinCelsiusRounded(x)
           }]
         },
         options: {
+          scales: {
+            yAxes: [{
+               scaleLabel: {
+                    display: true,
+                    labelString: 'temperature[°C]',
+                  },
+                ticks: {
+                    max: getMaxValue(x),
+                    min: getMinValue(x),
+                }
+             }]
+          },
           title: {
             display: true,
-            text: 'Forecast for five days',
+            text: 'Temperature for five days',
             fontSize: 25
           },
           legend: {
@@ -203,6 +280,77 @@ function chartForWeather(x){
           }
         }
       });
+    });
+}
 
+
+function chartForWeatherSecond(x){
+    $(document).ready(function() {
+      let myChart = document.getElementById('myChart').getContext('2d');
+
+      Chart.defaults.globalFontFamily= 'Ariel';
+      Chart.defaults.globalFontSize= 15;
+      Chart.defaults.globalFontColor= '#fff';
+
+      let weatherChart = new Chart (myChartSecond, {
+        type:'line',
+        data: {
+          labels:removingCharactersFromString(x),
+          datasets:[{
+            fill: false,
+            lineTension: 0.1,
+            borderColor: "rgb(240, 44, 71)",
+            pointBorderColor: "rgb(171, 13, 34)",
+            pointBackgroundColor: "rgba(171, 13, 34, 0.8)",
+            label:'pressure',
+            data: pressureArray(x),
+            yAxisID: 'y-axis-1'},{
+              fill: false,
+              lineTension: 0.1,
+              borderColor: "rgb(118, 186, 208)",
+              pointBorderColor: "rgb(19, 81, 100)",
+              pointBackgroundColor: "rgba(19, 81, 100, 0.8)",
+              label:'wind speed',
+              data: speedWindArray(x),
+              yAxisID: 'y-axis-2'}
+            ],
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                   display: true,
+                   labelString: 'pressure[hPa]',
+                 },
+              type: 'linear',
+              display: true,
+              position: 'left',
+              id: 'y-axis-1',
+                  },
+                  {
+                  scaleLabel: {
+                       display: true,
+                       labelString: 'wind speed[km/h]',
+                     },
+  							type: 'linear',
+  							display: true,
+  							position: 'right',
+  							id: 'y-axis-2',
+  							gridLines: {
+  								drawOnChartArea: false,
+							}
+
+             }]
+          },
+          title: {
+            display: true,
+            text: 'Pressure and speed wind for five days',
+            fontSize: 25
+          },
+          legend: {
+            position: 'right'
+          }
+        }
+      });
     });
 }
